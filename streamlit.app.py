3bd2b1148802cff9ab1dee7c42a31ef6b33605b4
riljayanthi <114994887+riljayanthi@users.noreply.github.com>
2022-10-04 12:55:40 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -6,4 +6,16 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header('Breakfast Favourites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
+
+streamlit.title("Build Your Own Fruit Smoothie")
+streamlit.header('Breakfast Favourites')
+streamlit.text('Orange,Banana & Apple Smoothie')
+streamlit.text('Egg Toast')
+streamlit.text('Rasberry & Strawberry Smoothie')
 
